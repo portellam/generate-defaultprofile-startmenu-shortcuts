b126bd1c58959180f511ab0dec5e5822af17c9a2
Alex Portell <amportell.13@gmail.com>
2023-10-13 23:41:58 -0400

    Update generate-appdata-shortcuts-for-startmenu.ps1
    Updated logic to create shortcuts for Default User only.

--- generate-appdata-shortcuts-for-startmenu.ps1
+++ generate-appdata-shortcuts-for-startmenu.ps1
@@ -1,11 +1,15 @@
-# Filename:    generate-appdata-shortcuts-for-startmenu.ps1
-# Description: Generates executable shortcuts from AppData\Local for the running User's StartMenu.
+# Filename:    generate-startmenu-shortcuts.ps1
+# Description: Generates executable shortcuts within Start Menu for the Default User.
 # Author(s):   Alex Portell <github.com/portellam>
 #
 
 $username = $Env:UserName
-Write-Host "Creating StartMenu shortcuts of AppData for $username..."
-$appdata_path = "C:\Users\$username\AppData"
+$default_username = "Default"
+$default_username_reference = '%username%'
+
+Write-Host "Creating StartMenu shortcuts..."
+
+$appdata_path = "C:\Users\$default_username\AppData"
 $local_path = "$appdata_path\Local\*\*"
 $local_path_results = New-Object System.Collections.ArrayList
 
@@ -21,11 +25,13 @@ foreach($file in Get-ChildItem $local_path)
 
 if($local_path_results.Count -le 0)
 {
-  exit
+  Write-Host "Skipped."
+  exit 0
 }
 
 $startmenu_path = "$appdata_path\Roaming\Microsoft\Windows\Start Menu"
 $shell = New-Object -ComObject WScript.Shell
+$has_passed_once = $false
 
 foreach($file in $local_path_results)
 {
@@ -35,11 +41,16 @@ foreach($file in $local_path_results)
     $shortcut_dir = $startmenu_path + "\" + $file_last_folder
     $shortcut_path = $shortcut_dir + "\" + $file.BaseName + ".lnk"
 
+    $username_temp = "\" + $username + "\"
+    $default_username_reference_temp = "\" + $default_username_reference + "\"
+    $shortcut_target = $file.FullName.Replace($username_temp, $default_username_reference_temp)
+
     $shortcut = $shell.CreateShortCut($shortcut_path)
-    $shortcut.TargetPath = $file.FullName
+    $shortcut.TargetPath = $shortcut_target
 
     New-Item -ItemType Directory -Path $shortcut_dir *>$null
     $shortcut.Save()
+    $has_passed_once = $true
   }
   catch
   {
@@ -48,5 +59,11 @@ foreach($file in $local_path_results)
   }
 }
 
-Write-Host "Done."
-exit
+if($has_passed_once -ne $true)
+{
+    Write-Host "Failed."
+    exit 1
+}
+
+Write-Host "Success."
+exit 0
